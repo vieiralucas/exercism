3f781711629dc8d391ea6157f90b0179c979b306
Lucas Vieira <vieiralucas4@gmail.com>
2017-08-10 00:53:00 -0300

    Elm scrabble-score

---
+++ elm/scrabble-score/ScrabbleScore.elm
@@ -0,0 +1,93 @@
+module ScrabbleScore exposing (..)
+
+import Char
+
+
+scoreChar : Char -> Int
+scoreChar c =
+    case Char.toUpper c of
+        'A' ->
+            1
+
+        'E' ->
+            1
+
+        'I' ->
+            1
+
+        'O' ->
+            1
+
+        'U' ->
+            1
+
+        'L' ->
+            1
+
+        'N' ->
+            1
+
+        'R' ->
+            1
+
+        'S' ->
+            1
+
+        'T' ->
+            1
+
+        'D' ->
+            2
+
+        'G' ->
+            2
+
+        'B' ->
+            3
+
+        'C' ->
+            3
+
+        'M' ->
+            3
+
+        'P' ->
+            3
+
+        'F' ->
+            4
+
+        'H' ->
+            4
+
+        'V' ->
+            4
+
+        'W' ->
+            4
+
+        'Y' ->
+            4
+
+        'K' ->
+            5
+
+        'J' ->
+            8
+
+        'X' ->
+            8
+
+        'Q' ->
+            10
+
+        'Z' ->
+            10
+
+        _ ->
+            0
+
+
+scoreWord : String -> Int
+scoreWord s =
+    String.toList s |> List.map scoreChar |> List.sum
